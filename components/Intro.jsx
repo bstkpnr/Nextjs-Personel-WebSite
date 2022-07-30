bfb92f3776961c1465e9e889ce2c1feea874b914
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-07-30 15:55:04 +0300

    added into page

---
+++ components/Intro.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Intro.module.css";
+const Intro = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.card}>first</div>
+      <div className={styles.card}>second</div>
+    </div>
+  );
+};
+
+export default Intro;
