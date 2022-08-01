43478f7e5e2d9b26ad9ca77ff29c7d53fda750b3
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-08-01 20:01:07 +0300

    create service page and css

---
+++ components/Services.jsx
@@ -0,0 +1,37 @@
+import styles from "../styles/Services.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+const Services = ({ services }) => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>What I Can Do?</h1>
+      <h1 className={styles.title}>What I Can Do?</h1>
+      <div className={styles.services}>
+        {services.map((service) => (
+          <Link passHref key={service.id} href={`/products/${service.name}`}>
+            <div className={styles.service}>
+                <div className={styles.desc}>{service.desc}</div>
+              <span className={styles.category}>{service.title}</span>
+              <div className={styles.media}>
+                {service.video ? (
+                  <video
+                    src={`/img/${service.video}`}
+                    autoPlay
+                    muted
+                    loop
+                    className={styles.video}
+                  ></video>
+                ) : (
+                  <Image src={`/img/${service.photo}`} alt="" className={styles.photo} width="100%" height="100%" layout="responsive" objectFit="cover" />
+                )}
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Services;
