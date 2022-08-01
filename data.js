45ebc2b5c7427ef778e6d5855ddd9af004a59521
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-08-01 19:59:43 +0300

    create data

---
+++ data.js
@@ -0,0 +1,31 @@
+export const data=[
+    {
+        id:1,
+        name:"developer",
+        title: "Developer",
+        desc: "Açıklama Yokk",
+        photo: "devbeste.jpeg"
+
+    },
+    {
+        id:2,
+        name: "art",
+        title: "ART",
+        desc: "Gezgin Açıklama",
+        photo: "art.jpeg"
+    },
+    {
+        id:3,
+        name: "music",
+        title: "Music",
+        desc: "Music Açıklama",
+        photo: "spotify.png"
+    },
+    {
+        id:4,
+        name: "food",
+        title: "Food",
+        desc: "Food Açıklama",
+        photo: "before.jpg"
+    }
+]
