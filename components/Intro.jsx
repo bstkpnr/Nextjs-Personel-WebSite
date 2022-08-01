ee192fab4a7ef9b18102e33af56e3db35669e6db
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-08-01 15:47:44 +0300

    edit intro page and styles

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,9 +1,18 @@
 import styles from "../styles/Intro.module.css";
+import Image  from "next/image"
 const Intro = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.card}>first</div>
-      <div className={styles.card}>second</div>
+      <div className={styles.card}>
+        <h1 className={styles.title}><span className={styles.brand}>BESTE</span> BESTE TOKPINAR</h1>
+        <p className={styles.desc}>
+          Okur,Öğrenir,İnşaa Eder
+        </p>
+        <button className={styles.button}>DISCOVER</button>
+      </div>
+      <div className={styles.card}>
+     <Image src="/img/kaos.png" alt="" width="100%" height="100%" layout="fill" objectFit="cover" />
+      </div>
     </div>
   );
 };
