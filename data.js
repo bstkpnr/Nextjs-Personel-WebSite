29498de493b66c9556c0c939bcd32a22726175f9
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2022-08-05 14:15:35 +0300

    added images data

--- data.js
+++ data.js
@@ -1,32 +1,104 @@
 export const data=[
     {
         id:1,
-        name:"developer",
+        name:"about",
         title: "Developer",
         desc: "Açıklama Yokk",
-        photo: "devbeste.jpeg"
+        photo: "devbeste.jpeg",
+        images: [
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/9965121/pexels-photo-9965121.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 2,
+              url: "https://images.pexels.com/photos/9716799/pexels-photo-9716799.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 3,
+              url: "https://images.pexels.com/photos/9395308/pexels-photo-9395308.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 4,
+              url: "https://images.pexels.com/photos/10067102/pexels-photo-10067102.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            }
+        ]
 
     },
     {
         id:2,
-        name: "art",
-        title: "ART",
+        name: "blog",
+        title: "BLOG",
         desc: "Gezgin Açıklama",
-        photo: "art.jpeg"
+        photo: "art.jpeg",
+        images: [
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/9965121/pexels-photo-9965121.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 2,
+              url: "https://images.pexels.com/photos/9716799/pexels-photo-9716799.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 3,
+              url: "https://images.pexels.com/photos/9395308/pexels-photo-9395308.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 4,
+              url: "https://images.pexels.com/photos/10067102/pexels-photo-10067102.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            }
+        ]
     },
     {
         id:3,
-        name: "music",
+        name: "playlist",
         title: "Music",
         desc: "Music Açıklama",
-        photo: "spotify.png"
+        photo: "spotify.png",
+        images: [
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/9965121/pexels-photo-9965121.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 2,
+              url: "https://images.pexels.com/photos/9716799/pexels-photo-9716799.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 3,
+              url: "https://images.pexels.com/photos/9395308/pexels-photo-9395308.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 4,
+              url: "https://images.pexels.com/photos/10067102/pexels-photo-10067102.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            }
+        ]
     },
     {
         id:4,
         name: "food",
         title: "Food",
         desc: "Food Açıklama",
-        photo: "before.jpg"
+        photo: "before.jpg",
+        images: [
+            {
+              id: 1,
+              url: "https://images.pexels.com/photos/9965121/pexels-photo-9965121.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 2,
+              url: "https://images.pexels.com/photos/9716799/pexels-photo-9716799.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 3,
+              url: "https://images.pexels.com/photos/9395308/pexels-photo-9395308.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            },
+            {
+              id: 4,
+              url: "https://images.pexels.com/photos/10067102/pexels-photo-10067102.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+            }
+        ]
     }
 ]
 
@@ -50,3 +122,4 @@ export const users=[
         title:"Bilgisayar Mühendisi",
     }
 ]
+
